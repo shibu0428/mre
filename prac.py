8ba9bd1677e0968344874f5c00ef07feb6cab7eb
shibu0428 <152146579+shibu0428@users.noreply.github.com>
2024-04-26 15:40:57 +0900

    Update prac.py

--- prac.py
+++ prac.py
@@ -2,7 +2,7 @@ import numpy as np
 import readfile as rf
 import partsset as ps
 
-fp='samples/sample_int.txt'
+fp='sample/sample_int.txt'
 qdata,xdata=rf.file_sep(fp)
 #print(qdata)
 cut_qdata=ps.pts_cut(qdata,[0,1,2,3,24])
