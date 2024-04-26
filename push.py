65cc886c075172959ae912db6ccd0505d44dcd8a
shibu <t23m003@mail.ryukoku.ac.jp>
2024-04-26 14:27:13 +0900

    yey

--- push.py
+++ push.py
@@ -5,8 +5,9 @@ from datetime import datetime
 print("koko")
 now = datetime.now()
 comment = datetime.now().strftime("%Y/%m/%d-%H:%M:%S")
-#comment = input("コメント入力")
+comment = input("コメント入力")
 # Push
+comment='"'+comment+'"'
 os.system('git add .')
-os.system('git commit -m {}'.format(comment))
+os.system('git commit -m '+comment)
 os.system('git push')
