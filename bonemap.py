e30ad7c855d307b66dac5c3c25c31f88ba5e7d83
shibu <t23m003@mail.ryukoku.ac.jp>
2024-04-30 17:22:30 +0900

    soc改造開始

---
+++ bonemap.py
@@ -0,0 +1,49 @@
+#変数定義import用ファイル
+#bone_mapはボーンの名称を添え字番号で表現している
+#
+
+bone_map = [
+    "Hips", #0
+    "Spine", #1
+    None, #2
+    "Chest", #3
+    None, #4
+    "UpperChest", #5
+    None, #6
+    "-", #7
+    "-", #8
+    "Neck", #9
+    "Head", #10
+    "RightShoulder", #11
+    "RightUpperArm", #12
+    "RightLowerArm", #13
+    "RightHand", #14
+    "LeftShoulder", #15
+    "LeftUpperArm", #16
+    "LeftLowerArm", #17
+    "LeftHand", #18
+    "RightUpperLeg", #19
+    "RightLowerLeg", #20
+    "RightFoot", #21
+    "RightToes", #22
+    "LeftUpperLeg", #23
+    "LeftLowerLeg", #24
+    "LeftFoot", #25
+    "LeftToes" #26
+]
+
+parent_path=[
+    None,#root
+    0,#1.上半身開始
+    1,#2
+    2,#3
+    3,#4
+    4,#5
+    5,#6
+    6,#7
+    7,#8
+    8,#9
+    9,#10
+    7,#11
+    11#
+    ]
