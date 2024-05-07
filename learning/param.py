ff7fa11d7f440d2d9385e07b83f80ed7c3f96ecf
shibu <t23m003@mail.ryukoku.ac.jp>
2024-05-07 15:38:23 +0900

    param更新

--- learning/param.py
+++ learning/param.py
@@ -14,7 +14,13 @@ fp="../dataset/"
 motions={
     0:"walk",
     1:"suburi",
-    2:"udehuri"
+    2:"udehuri",
+    3:"iai",
+    4:"sit_stop",
+    5:"sit_udehuri",
+    6:"stand_neutral",
+    7:"scwat",
+    8:"fencing_stand",
 }
 
 
