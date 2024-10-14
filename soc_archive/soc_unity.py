d45281f7d93dc6e9b630e5ab1d5dc8025ba907b7
shibu0428 <t23m003@mail.ryukoku.ac.jp>
2024-10-14 11:01:11 +0900

    soc

---
+++ soc_archive/soc_unity.py
@@ -0,0 +1,14 @@
+import socket
+import struct
+
+# UDPの受信設定
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.bind(("133.83.82.105", 5002))  # IPアドレスを指定してバインド
+
+
+print("Waiting for UDP data...")
+
+while True:
+    data, addr = udp_socket.recvfrom(1024)  # データを受信
+    x, y, z = struct.unpack('fff', data[:12])  # 受信したデータからX, Y, Z座標を抽出
+    print(f"Received position: X={x}, Y={y}, Z={z} from {addr}")
