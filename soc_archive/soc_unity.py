34043cb512d16f63f57d22d8456fd1355b854623
shibu0428 <t23m003@mail.ryukoku.ac.jp>
2024-10-28 03:44:52 +0900

    tanomu

--- soc_archive/soc_unity.py
+++ soc_archive/soc_unity.py
@@ -3,12 +3,30 @@ import struct
 
 # UDPの受信設定
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind(("10.20.153.194", 5002))  # IPアドレスを指定してバインド
+udp_socket.bind(("192.168.10.110", 5002))  # IPアドレスを指定してバインド
 
+filename=input("ファイル名")
+print("Waiting for UDP data")
+with open(filename+'.csv',mode='a') as f:
+    while True:
+        data, addr = udp_socket.recvfrom(1024)  # データを受信
+        for i in range(6):
+            f.write(str(struct.unpack('<ffffffff', data[i*40:i*40+32])).replace(")(", ",").replace("(", "").replace(")", ""))
+            f.write(",")
+            f.write(str(struct.unpack('<q', data[i*40+32:i*40+40])).replace(")(", ",").replace("(", "").replace(")", ""))
+            #print(struct.unpack('<q', data[i*40+32:i*40+40]))
+        f.write(f"\n")
 
-print("Waiting for UDP data...")
-
-while True:
-    data, addr = udp_socket.recvfrom(1024)  # データを受信
-    x, y, z = struct.unpack('fff', data[:12])  # 受信したデータからX, Y, Z座標を抽出
-    print(f"Received position: X={x}, Y={y}, Z={z} from {addr}")
+'''
+print("Waiting for UDP data")
+with open(filename+'.csv',mode='a') as f:
+    while True:
+        data, addr = udp_socket.recvfrom(1024)  # データを受信
+        for i in range((int(len(data)/4))):
+            if i%9 == 8:
+                f.write(str(struct.unpack('<f', data[i*4:i*4+4])).replace(")(", ",").replace("(", "").replace(")", ""))
+            else:
+                f.write(str(struct.unpack('<f', data[i*4:i*4+4])).replace(")(", ",").replace("(", "").replace(")", ""))
+        f.write(f"\n")
+'''        
+        
