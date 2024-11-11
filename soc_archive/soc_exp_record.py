f14a2130f4e8c75762b00877c00bbe603e4ed94b
shibu0428 <t23m003@mail.ryukoku.ac.jp>
2024-11-11 11:05:45 +0900

    1111_datarecorded

--- soc_archive/soc_exp_record.py
+++ soc_archive/soc_exp_record.py
@@ -3,19 +3,23 @@ import struct
 
 # UDPの受信設定
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind(("192.168.10.110", 5002))  # IPアドレスを指定してバインド
+udp_socket.bind(("192.168.68.54", 5002))  # IPアドレスを指定してバインド
 
 filename=input("ファイル名")
 print("Waiting for UDP data")
 nframe=0
-dataframe=5000
+dataframe=2000
 minframe=300
 
 maxframe=dataframe+minframe
 
+
+
 with open(filename+'.csv',mode='a') as f:
     while True:
         data, addr = udp_socket.recvfrom(1024)  # データを受信
+        if nframe == 0:
+            print("300フレーム後に開始します")
         nframe+=1
 
         #開始のフレームより早いなら処理しない
@@ -30,7 +34,8 @@ with open(filename+'.csv',mode='a') as f:
                   "終了します\n")
             f.close()
             exit()
-
+        if nframe%50 == 0:
+            print(nframe-300)
         #バイト列を処理して書き込み
         for i in range(6):
             f.write(str(struct.unpack('<ffffffff', data[i*40:i*40+32])).replace(")(", ",").replace("(", "").replace(")", ""))
